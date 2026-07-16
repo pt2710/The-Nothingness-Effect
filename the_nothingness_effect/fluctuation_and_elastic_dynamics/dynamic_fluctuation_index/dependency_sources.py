66df120d2da46285eae5d6fed8ec36782707038c
pt2710 <132405221+pt2710@users.noreply.github.com>
2026-07-16 20:25:55 +0200

    Add DFI dependency-closing source contracts

---
+++ the_nothingness_effect/fluctuation_and_elastic_dynamics/dynamic_fluctuation_index/dependency_sources.py
@@ -0,0 +1,9 @@
+"""Dependency-closing A-level DFI source contracts."""
+from the_nothingness_effect._runtime.theorem_complex_runtime.dependency_sources import contracts_for
+
+
+def contracts():
+    return contracts_for(
+        "dfi",
+        "the_nothingness_effect/fluctuation_and_elastic_dynamics/dynamic_fluctuation_index/dependency_sources.py",
+    )

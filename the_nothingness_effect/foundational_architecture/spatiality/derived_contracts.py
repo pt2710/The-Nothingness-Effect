af6d133cc04eeda150b6815179a1fd3f80405289
pt2710 <132405221+pt2710@users.noreply.github.com>
2026-07-16 21:04:08 +0200

    Recertify foundational Spatiality source binding

--- the_nothingness_effect/foundational_architecture/spatiality/derived_contracts.py
+++ the_nothingness_effect/foundational_architecture/spatiality/derived_contracts.py
@@ -7,22 +7,31 @@ from the_nothingness_effect._runtime.theorem_complex_runtime.derived_laws import
 
 
 APPENDIX = "appendix_tne_foundational_closure_architecture.tex"
-APPENDIX_SHA256 = "2679b61a1d98100ed3a13669c16c299cd9b09807bc3847d383d559c9251189ea"
-IMPLEMENTATION = "the_nothingness_effect/foundational_architecture/spatiality/derived_contracts.py"
+APPENDIX_SHA256 = "5e459eed3eca36d1342bc879fc8ac3962f3c801bfd1aab733f3db081a7ed0c69"
+IMPLEMENTATION = (
+    "the_nothingness_effect/foundational_architecture/"
+    "spatiality/derived_contracts.py"
+)
 
 
 def contracts():
     return (
         additive_contract(
             "involution_to_chord_linearization",
-            ("order_two_symmetry_recursion", "affine_spatial_involution_orbit"),
+            (
+                "order_two_symmetry_recursion",
+                "affine_spatial_involution_orbit",
+            ),
             appendix=APPENDIX,
             appendix_sha256=APPENDIX_SHA256,
             implementation_path=IMPLEMENTATION,
         ),
         spatial_contract(
             "twisted_orbit_balance_bundle",
-            ("phase_gated_compensation_transport", "involution_to_chord_linearization"),
+            (
+                "phase_gated_compensation_transport",
+                "involution_to_chord_linearization",
+            ),
             appendix=APPENDIX,
             appendix_sha256=APPENDIX_SHA256,
             implementation_path=IMPLEMENTATION,
